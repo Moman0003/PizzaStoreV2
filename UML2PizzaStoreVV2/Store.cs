52776c1665e69183bbae77615fecf8983d8c3234
momans <mos003@edu.zealand.dk>
2024-04-03 02:50:33 +0200

    PizzaStoreV2

---
+++ UML2PizzaStoreVV2/Store.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UML_2_PizzaStoreV2;
+
+namespace UML_2_PizzaStoreV2
+{
+    public class Store
+    {
+        private MenuCatalog _menuCatalog;
+
+        public Store()
+        {
+            _menuCatalog = new MenuCatalog();
+        }
+
+        public void Test()
+        {
+            Pizza pizza1 = new Pizza() { Number = 1, Name = "Margherita", Price = 500 };
+            _menuCatalog.CreatePizza(pizza1);
+
+            Pizza pizza2 = new Pizza() { Number = 2, Name = "Pepperoni", Price = 60 };
+            _menuCatalog.CreatePizza(pizza2);
+
+            _menuCatalog.PrintMenu();
+
+            Pizza foundPizza = _menuCatalog.FindPizza(1);
+            if (foundPizza != null)
+            {
+                // Håndter tilfælde hvor en pizza blev fundet
+                Console.WriteLine("Pizza found: " + foundPizza.Name);
+            }
+            else
+            {
+                // Håndter tilfælde hvor ingen pizza blev fundet
+                Console.WriteLine("No pizza found.");
+            }
+        }
+    }
+}
---
+++ UML2PizzaStoreVV2/UserDialog.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using UML_2_PizzaStoreV2;
+
+namespace UML_2_PizzaStoreV2
+{
+    public class UserDialog
+    {
+        private MenuCatalog _menuCatalog;
+
+        public UserDialog(MenuCatalog menuCatalog)
+        {
+            _menuCatalog = menuCatalog;
+        }
+
+        public void Run()
+        {
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine("1. Add new pizza");
+                Console.WriteLine("2. Delete pizza");
+                Console.WriteLine("3. Update pizza");
+                Console.WriteLine("4. Search pizza");
+                Console.WriteLine("5. Display pizza menu");
+                Console.WriteLine("6. Quit");
+                Console.Write("Select an option: ");
+                string? input = Console.ReadLine();
+                if (input != null)
+                {
+                    switch (input)
+                    {
+                        case "1":
+                            AddPizza();
+                            break;
+                        case "2":
+                            DeletePizza();
+                            break;
+                        case "3":
+                            UpdatePizza();
+                            break;
+                        case "4":
+                            SearchPizza();
+                            break;
+                        case "5":
+                            DisplayMenu();
+                            break;
+                        case "6":
+                            running = false;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid option. Please try again.");
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void AddPizza()
+        {
+            // Logic for adding a new pizza
+        }
+
+        private void DeletePizza()
+        {
+            // Logic for deleting a pizza
+        }
+
+        private void UpdatePizza()
+        {
+            // Logic for updating a pizza
+        }
+
+        private void SearchPizza()
+        {
+            // Logic for searching for a pizza
+        }
+
+        private void DisplayMenu()
+        {
+            _menuCatalog.PrintMenu();
+        }
+    }
+}
